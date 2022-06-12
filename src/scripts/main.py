cc626c3559c29ddd936b07919872a4191ce7533c
종원 <56539682+Bruce0203@users.noreply.github.com>
2022-06-12 06:21:33 +0000

    Co-authored-by: 컨트롤D <copecone@users.noreply.github.com>

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -1,1 +1,21 @@
-print("Hello World")
+import numpy as np
+from matplotlib import pyplot as plt
+
+x = lambda x: x * 3 + 1 if x % 2 else x / 2
+
+li = []
+value = 837799
+while True:
+    li.append(int(value))
+    if value == 1: break
+    value = x(value)
+
+data = np.array(li, dtype=np.int64)
+
+fig, ax = plt.subplots()
+
+ax.plot(data)
+ax.set(xlabel='loop', ylabel='number')
+ax.grid()
+
+fig
