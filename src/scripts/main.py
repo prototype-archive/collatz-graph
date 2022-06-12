05f1df8eda1cd466538058f87e636254db1381fc
종원 <56539682+Bruce0203@users.noreply.github.com>
2022-06-12 07:59:24 +0000

    Co-authored-by: 컨트롤D <copecone@users.noreply.github.com>

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -3,19 +3,20 @@ from matplotlib import pyplot as plt
 
 x = lambda x: x * 3 + 1 if x % 2 else x / 2
 
-li = []
-value = 837799
-while True:
-    li.append(int(value))
-    if value == 1: break
-    value = x(value)
-
-data = np.array(li, dtype=np.int64)
-
 fig, ax = plt.subplots()
-
-ax.plot(data)
 ax.set(xlabel='loop', ylabel='number')
-ax.grid()
 
-fig
+number_input = Element("number_input")
+def make_graph(*args, **kwargs):
+    number_list = []
+    value = int(number_input.element.value)
+    while True:
+        number_list.append(int(value))
+        if value == 1: break
+        value = x(value)
+
+    data = np.array(number_list, dtype=np.int64)
+
+    ax.plot(data)
+    ax.grid()
+    fig
