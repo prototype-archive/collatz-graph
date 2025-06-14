3a7ec6a74187055cc46f3ab23f6c9ae7d3aad3d0
컨트롤DELTA <48048511+copecone@users.noreply.github.com>
2025-06-14 21:28:04 +0900

    Update main.py

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -1,6 +1,8 @@
 import re
 import numpy as np
 from matplotlib import pyplot as plt
+from pyweb import pydom
+from pyscript import display
 
 def calc_value(value):
     global cache
@@ -19,7 +21,7 @@ ax.set(xlabel='loop', ylabel='number')
 ax.grid()
 
 data_dict = {}
-number_input = Element("number_input")
+number_input = pydom["#number_input"][0]
 
 cache_size = int(1e7)
 cache = np.zeros(cache_size, dtype=np.uint64)
@@ -38,7 +40,7 @@ def make_graph(*args, **kwargs):
     global data_dict
 
     try:
-        value = int(number_input.element.value)
+        value = int(number_input.value)
     except ValueError as err:
         if len(args) > 0:
             value = int(args[0])
@@ -54,18 +56,18 @@ def make_graph(*args, **kwargs):
         data_dict[value] = [max(data), len(data)]
         write_plot(data)
 
-    Element("data_value").element.innerText = "Max value: {}\n{} loops".format(*data_dict[value])
+    pydom["#data_value"].innerText = "Max value: {}\n{} loops".format(*data_dict[value])
 
 def write_plot(data):
     ax.plot(data)
-    Element("plot").write(fig)
+    display(fig, target = "plot")
 
 def number_update(*args, **kwargs):
     try:
-        value = int(number_input.element.value)
+        value = int(number_input.value)
 
         if value < 2:
-            number_input.element.value = '2'
+            number_input.value = '2'
     except ValueError as err:
-        if number_input.element.value != '': number_input.element.value = re.sub(r'[^0-9]', '', number_input.element.value)
-        if number_input.element.value == '': number_input.element.value = '2'
+        if number_input.value != '': number_input.value = re.sub(r'[^0-9]', '', number_input.value)
+        if number_input.value == '': number_input.value = '2'
